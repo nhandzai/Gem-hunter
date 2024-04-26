e544a0ba121095e2304469ea5ed5e29c58bab142
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2024-04-26 20:54:47 +0700

    better

--- solve cnfs with pysat.py
+++ solve cnfs with pysat.py
@@ -2,6 +2,8 @@ from pysat.formula import *
 from pysat.solvers import *
 import ReadFile
 
+PATH = 'testcases\\input2.txt'
+
 class Cordinate():
     def __init__(self, x, y, m, n):
         self.top = max(x - 1, 0)
@@ -19,10 +21,6 @@ def generate_whatever_i_have_to(map_data: list):
         for j in range(0, n):
             if map_data[i][j].isdigit():
                 no_traps = int(map_data[i][j])
-                # special case
-                if no_traps == 0:
-                    
-                    pass
                 
                 if no_traps > 8:
                     raise Exception("Invalid input")
@@ -40,34 +38,55 @@ def generate_whatever_i_have_to(map_data: list):
                     print("No solution")
                     quit()
                     
-                # generate clauses
                 f = None
-                for comb in itertools.combinations(empty_cells, no_traps):
-                    # comb is a possible combination of cells that are traps
-                    traps = list(comb)
-                    gems = [cell for cell in empty_cells if cell not in traps]
-                    atoms = []
-                    
-                    for trap in traps:
-                        atoms.append(Atom('x_{}_{}'.format(trap[0], trap[1])))
-                    for gem in gems:
-                        atoms.append(Neg(Atom('x_{}_{}'.format(gem[0], gem[1]))))
+                atoms = []
+                clause = None
+                
+                # generate cnfs
+                if no_traps == 0:
+                    for cell in empty_cells:
+                        # all empty cells are gems
+                        atoms.append(Neg(Atom('x_{}_{}'.format(cell[0], cell[1]))))
                         
                     clause = And(atoms[0])
+                    
+                    if atoms[0] not in cnfs:
+                        cnfs.append(atoms[0])
+                        
                     for _ in range(1, len(atoms)):
-                        clause = clause & atoms[_]
+                        clause = clause & atoms[_]   
+                        if atoms[_] not in cnfs:
+                            cnfs.append(atoms[_])
+                    f = clause
                         
-                    if f is None:
-                        f = clause
-                    else:
-                        f = f | clause
+                else:
+                    for comb in itertools.combinations(empty_cells, no_traps):
+                        # comb is a possible combination of cells that are traps
+                        traps = list(comb)
+                        gems = [cell for cell in empty_cells if cell not in traps]
+                        atoms = []
                         
+                        for trap in traps:
+                            atoms.append(Atom('x_{}_{}'.format(trap[0], trap[1])))
+                        for gem in gems:
+                            atoms.append(Neg(Atom('x_{}_{}'.format(gem[0], gem[1]))))
+                            
+                        clause = And(atoms[0])
+                        for _ in range(1, len(atoms)):
+                            clause = clause & atoms[_]
+                            
+                        if f is None:
+                            f = clause
+                        else:
+                            f = f | clause
+                            
                 if formula is None:
                     formula = f
                 else: 
                     formula = formula & f
-                    
-                cnfs.append(f)
+
+                if no_traps != 0:
+                    cnfs.append(f)
 
     return formula, cnfs
 
@@ -98,19 +117,22 @@ def solve(map_data: list, formula):
     for row in map_data:
         print(row)
 
-path = 'input.txt'
-map_data = ReadFile.read_map(path)
-formula, cnfs = generate_whatever_i_have_to(map_data)
-formula.clausify()
+def main():
+    map_data = ReadFile.read_map(PATH)
+    formula, cnfs = generate_whatever_i_have_to(map_data)
+    formula.clausify()
 
-# temp = formula.export_vpool().id2obj
-# for key, value in temp.items():
-#     print(f"{key}: {value}")
-# print()
+    # temp = formula.export_vpool().id2obj
+    # for key, value in temp.items():
+    #     print(f"{key}: {value}")
+    # print()
 
-for i, cnf in enumerate(cnfs):
-    print(f"{i + 1}: {cnf}")
+    for i, cnf in enumerate(cnfs):
+        print(f"{i + 1}: {cnf}")
+        print()
     print()
-print()
 
-solve(map_data, formula)
+    solve(map_data, formula)
+    
+if __name__ == '__main__':
+    main()
