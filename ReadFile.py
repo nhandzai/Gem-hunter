6d04916aa5f50f59b85d7bc0a71893b62a298fd4
nhandzai <ducnhan1522004@gmail.com>
2024-04-27 20:14:53 +0700

    idk why the cnf cant run with the new read_file

--- ReadFile.py
+++ ReadFile.py
@@ -10,3 +10,15 @@ def read_map(path: str) -> list:
             map_data.append(row)
         
     return map_data
+
+def read_map2(path: str):
+    map_data = []
+    empty_pos = []
+    with open(path, 'r') as file:
+        for row_index, line in enumerate(file):
+            row = line.strip('\n').split(', ')
+            map_data.append(row)
+            for col_index, cell in enumerate(row):
+                if cell == '_':
+                    empty_pos.append((row_index, col_index))
+    return map_data, empty_pos
